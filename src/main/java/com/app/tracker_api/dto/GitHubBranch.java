581e1d0b467ef817a84961f9f988328d76e29cc8
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2025-03-06 21:55:22 +0100

    json fields format changed

--- src/main/java/com/app/tracker_api/dto/GitHubBranch.java
+++ src/main/java/com/app/tracker_api/dto/GitHubBranch.java
@@ -1,5 +1,6 @@
 package com.app.tracker_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,5 +8,6 @@ import lombok.Getter;
 @Getter
 public class GitHubBranch {
     private final String name;
+    @JsonProperty("last_commit_sha")
     private final String lastCommitSha;
 }
--- src/main/java/com/app/tracker_api/dto/GitHubRepository.java
+++ src/main/java/com/app/tracker_api/dto/GitHubRepository.java
@@ -1,5 +1,6 @@
 package com.app.tracker_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Getter
 public class GitHubRepository {
     private final String name;
+    @JsonProperty("owner_login")
     private final String ownerLogin;
     private final List<GitHubBranch> branches;
 }
