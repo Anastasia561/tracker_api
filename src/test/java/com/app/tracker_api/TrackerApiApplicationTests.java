11c7824ed419274d39d9837521fb6712fe2f643d
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2025-03-06 21:28:15 +0100

    initial commit

---
+++ src/test/java/com/app/tracker_api/TrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
